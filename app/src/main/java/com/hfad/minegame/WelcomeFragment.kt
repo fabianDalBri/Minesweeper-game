8ebd5ab01f7b48b67af292600110f9e088dd0469
fabian <fabiandahlin70@gmail.com>
2023-12-14 13:09:11 +0100

    initial commit

--- app/src/main/java/com/hfad/minegame/WelcomeFragment.kt
+++ app/src/main/java/com/hfad/minegame/WelcomeFragment.kt
@@ -13,7 +13,6 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
         val startButton = view.findViewById<Button>(R.id.start_game)
-
         startButton.setOnClickListener {
         view.findNavController().navigate(R.id.action_welcomeFragment_to_gameFragment)
 
